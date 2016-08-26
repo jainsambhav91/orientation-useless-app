2d4e6e99bc7d87574644cd2317875f7e9f4e19d8
jainsambhav91 <jain.sambhav91@gmail.com>
2016-08-26 15:45:40 -0400

    Update app.js
    third commit

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Today is a great day for data visualization.\n');
+    res.end('Today is a great day.\n');
 }).listen(process.env.PORT, process.env.IP);
